c513c225bdc8ac50fe4e8008012efc470f5f7e40
Yuki Tawara <yktawara@gmail.com>
2017-10-25 01:10:41 +0900

    Override hashCode and equals method in BoardPosition

--- src/main/java/com/example/carcassonne/BoardPosition.java
+++ src/main/java/com/example/carcassonne/BoardPosition.java
@@ -6,15 +6,29 @@ public class BoardPosition {
     private int y;
 
     public BoardPosition(int x, int y) {
-	this.x = x;
-	this.y = y;
+        this.x = x;
+        this.y = y;
     }
 
     public int getX() {
-	return x;
+        return x;
     }
 
     public int getY() {
-	return y;
+        return y;
+    }
+
+    @Override
+    public int hashCode() {
+        return x + 31 * y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o instanceof BoardPosition) {
+            BoardPosition that = (BoardPosition) o;
+            return x == that.x && y == that.y;
+        }
+        return false;
     }
 }
