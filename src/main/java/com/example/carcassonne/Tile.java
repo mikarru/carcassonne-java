a56199562ed526be80d57e7832122c359e816e96
Yuki Tawara <yktawara@gmail.com>
2017-10-26 01:46:18 +0900

    Fix style

--- src/main/java/com/example/carcassonne/Tile.java
+++ src/main/java/com/example/carcassonne/Tile.java
@@ -20,121 +20,121 @@ public class Tile {
 
     public Tile(int id, String name, BorderType[] borderTypes,
             Segment[] citySegments, Segment[] roadSegments,
-	    Segment[] fieldSegments, Segment cloisterSegment,
-		int[] cities, int[] roads, int[] fields) {
-	this.id = id;
-	this.name = name;
-	this.citySegments = citySegments;
-	this.roadSegments = roadSegments;
-	this.cloisterSegment = cloisterSegment;
-	this.cities = cities;
-	this.roads = roads;
-	this.fields = fields;
-	for (Segment s : citySegments) {
-	    s.setTile(this);
-	}
-	for (Segment s : roadSegments) {
-	    s.setTile(this);
-	}
-	for (Segment s : fieldSegments) {
-	    s.setTile(this);
-	}
-	if (cloisterSegment != null) {
-	    cloisterSegment.setTile(this);
-	}
+            Segment[] fieldSegments, Segment cloisterSegment,
+                int[] cities, int[] roads, int[] fields) {
+        this.id = id;
+        this.name = name;
+        this.citySegments = citySegments;
+        this.roadSegments = roadSegments;
+        this.cloisterSegment = cloisterSegment;
+        this.cities = cities;
+        this.roads = roads;
+        this.fields = fields;
+        for (Segment s : citySegments) {
+            s.setTile(this);
+        }
+        for (Segment s : roadSegments) {
+            s.setTile(this);
+        }
+        for (Segment s : fieldSegments) {
+            s.setTile(this);
+        }
+        if (cloisterSegment != null) {
+            cloisterSegment.setTile(this);
+        }
     }
 
     public int getId() {
-	return id;
+        return id;
     }
 
     public String getName() {
-	return name;
+        return name;
     }
 
     public Tile setX(int x) {
-	this.x = x;
-	return this;
+        this.x = x;
+        return this;
     }
 
     public Tile setY(int y) {
-	this.y = y;
-	return this;
+        this.y = y;
+        return this;
     }
 
     public int getX() {
-	return x;
+        return x;
     }
 
     public int getY() {
-	return y;
+        return y;
     }
 
     public Tile setRotation(int rotation) {
-	this.rotation = rotation;
-	return this;
+        this.rotation = rotation;
+        return this;
     }
 
     public Segment getCitySegmentOfDirection(int direction) {
-	int index = cities[direction];
-	return (index != -1) ? citySegments[index] : null;
+        int index = cities[direction];
+        return (index != -1) ? citySegments[index] : null;
     }
 
     public Segment getRoadSegmentOfDirection(int direction) {
-	int index = roads[direction];
-	return (index != -1) ? roadSegments[index] : null;
+        int index = roads[direction];
+        return (index != -1) ? roadSegments[index] : null;
     }
 
     public Segment getFieldSegmentOfDirection(int direction) {
-	int index = fields[direction];
-	return (index != -1) ? fieldSegments[index] : null;
+        int index = fields[direction];
+        return (index != -1) ? fieldSegments[index] : null;
     }
 
     public Segment[] getCitySegments() {
-	return citySegments;
+        return citySegments;
     }
 
     public Segment[] getRoadSegments() {
-	return roadSegments;
+        return roadSegments;
     }
 
     public Segment[] getFieldSegments() {
-	return fieldSegments;
+        return fieldSegments;
     }
 
     public Segment getCloisterSegment() {
-	return cloisterSegment;
+        return cloisterSegment;
     }
 
     public int getRotation() {
-	return rotation;
+        return rotation;
     }
 
     public BorderType getBorderType(int d) {
-	return borderTypes[d];
+        return borderTypes[d];
     }
 
     // topTileをこのタイルの上に回転rした状態でで置くことができるかどうか
     public boolean canTopAdjacentWith(Tile topTile, int r) {
-	// TODO
-	return false;
+        // TODO
+        return false;
     }
 
     // bottomTileをこのタイルの下に回転rした状態で置くことができるかどうか
     public boolean canBottomAdjacentWith(Tile bottomTile, int r) {
-	// TODO
-	return false;
+        // TODO
+        return false;
     }
 
     // rightTileをこのタイルの右に回転rした状態で置くことができるかどうか
     public boolean canRightAdjacentWith(Tile rightTile, int r) {
-	// TODO
-	return false;
+        // TODO
+        return false;
     }
 
     // leftTileをこのタイルの左に回転rした状態で置くことができるかどうか
     public boolean canLeftAdjacentWith(Tile leftTile, int r) {
-	// TODO
-	return false;
+        // TODO
+        return false;
     }
 }
--- src/main/java/com/example/carcassonne/Tiles.java
+++ src/main/java/com/example/carcassonne/Tiles.java
@@ -7,154 +7,154 @@ import static com.example.carcassonne.BorderType.ROAD;
 
 public class Tiles {
     public static Tile newA(int id) {
-	final String name = "A";
-	BorderType[] borderTypes = new BorderType[] {
-	    FIELD, FIELD, ROAD, FIELD
-	};
-	Segment[] citySegments = new Segment[0];
-	Segment[] roadSegments = new Segment[] {Segment.newRoad(0)};
-	Segment[] fieldSegments = new Segment[] {Segment.newField(0)};
-	Segment cloisterSegment = Segment.newCloister(0);
-	int[] cities = new int[] {-1, -1, -1, -1};
-	int[] roads = new int[] {-1, -1, 0, -1};
-	int[] fields = new int[] {0, 0, 0, 0, 0, 0, 0, 0};
-	return new Tile(id, name, borderTypes,
-	        citySegments, roadSegments, fieldSegments, cloisterSegment,
-		cities, roads, fields);
+        final String name = "A";
+        BorderType[] borderTypes = new BorderType[] {
+            FIELD, FIELD, ROAD, FIELD
+        };
+        Segment[] citySegments = new Segment[0];
+        Segment[] roadSegments = new Segment[] {Segment.newRoad(0)};
+        Segment[] fieldSegments = new Segment[] {Segment.newField(0)};
+        Segment cloisterSegment = Segment.newCloister(0);
+        int[] cities = new int[] {-1, -1, -1, -1};
+        int[] roads = new int[] {-1, -1, 0, -1};
+        int[] fields = new int[] {0, 0, 0, 0, 0, 0, 0, 0};
+        return new Tile(id, name, borderTypes,
+                citySegments, roadSegments, fieldSegments, cloisterSegment,
+                cities, roads, fields);
     }
 
     public static Tile newB(int id) {
-	final String name = "B";
-	BorderType[] borderTypes = new BorderType[] {
-	    FIELD, FIELD, FIELD, FIELD
-	};
-	Segment[] citySegments = new Segment[0];
-	Segment[] roadSegments = new Segment[0];
-	Segment[] fieldSegments = new Segment[] {Segment.newField(0)};
-	Segment cloisterSegment = Segment.newCloister(0);
-	int[] cities = new int[] {-1, -1, -1, -1};
-	int[] roads = new int[] {-1, -1, -1, -1};
-	int[] fields = new int[] {0, 0, 0, 0, 0, 0, 0, 0};
-	return new Tile(id, name, borderTypes,
-	        citySegments, roadSegments, fieldSegments, cloisterSegment,
-		cities, roads, fields);
+        final String name = "B";
+        BorderType[] borderTypes = new BorderType[] {
+            FIELD, FIELD, FIELD, FIELD
+        };
+        Segment[] citySegments = new Segment[0];
+        Segment[] roadSegments = new Segment[0];
+        Segment[] fieldSegments = new Segment[] {Segment.newField(0)};
+        Segment cloisterSegment = Segment.newCloister(0);
+        int[] cities = new int[] {-1, -1, -1, -1};
+        int[] roads = new int[] {-1, -1, -1, -1};
+        int[] fields = new int[] {0, 0, 0, 0, 0, 0, 0, 0};
+        return new Tile(id, name, borderTypes,
+                citySegments, roadSegments, fieldSegments, cloisterSegment,
+                cities, roads, fields);
     }
 
     public static Tile newC(int id) {
-	final String name = "C";
-	BorderType[] borderTypes = new BorderType[] {
-	    CITY, CITY, CITY, CITY
-	};
-	Segment[] citySegments = new Segment[] {Segment.newCity(0, true)};
-	Segment[] roadSegments = new Segment[0];
-	Segment[] fieldSegments = new Segment[0];
-	Segment cloisterSegment = null;
-	int[] cities = new int[] {0, 0, 0, 0};
-	int[] roads = new int[] {-1, -1, -1, -1};
-	int[] fields = new int[] {-1, -1, -1, -1, -1, -1, -1, -1};
-	return new Tile(id, name, borderTypes,
-	        citySegments, roadSegments, fieldSegments, cloisterSegment,
-		cities, roads, fields);
+        final String name = "C";
+        BorderType[] borderTypes = new BorderType[] {
+            CITY, CITY, CITY, CITY
+        };
+        Segment[] citySegments = new Segment[] {Segment.newCity(0, true)};
+        Segment[] roadSegments = new Segment[0];
+        Segment[] fieldSegments = new Segment[0];
+        Segment cloisterSegment = null;
+        int[] cities = new int[] {0, 0, 0, 0};
+        int[] roads = new int[] {-1, -1, -1, -1};
+        int[] fields = new int[] {-1, -1, -1, -1, -1, -1, -1, -1};
+        return new Tile(id, name, borderTypes,
+                citySegments, roadSegments, fieldSegments, cloisterSegment,
+                cities, roads, fields);
     }
     
     public static Tile newD(int id) {
-	final String name = "D";
-	BorderType[] borderTypes = new BorderType[] {
-	    ROAD, CITY, ROAD, FIELD
-	};
-	Segment[] citySegments = new Segment[] {Segment.newCity(0, false)};
-	Segment[] roadSegments = new Segment[] {Segment.newRoad(0)};
-	Segment[] fieldSegments = new Segment[] {Segment.newField(0), Segment.newField(1)};
-	Segment cloisterSegment = null;
-	int[] cities = new int[] {-1, 0, -1, -1};
-	int[] roads = new int[] {0, -1, 0, -1};
-	int[] fields = new int[] {0, 1, -1, -1, 1, 0, 0, 0};
-	return new Tile(id, name, borderTypes,
-	        citySegments, roadSegments, fieldSegments, cloisterSegment,
-		cities, roads, fields);
+        final String name = "D";
+        BorderType[] borderTypes = new BorderType[] {
+            ROAD, CITY, ROAD, FIELD
+        };
+        Segment[] citySegments = new Segment[] {Segment.newCity(0, false)};
+        Segment[] roadSegments = new Segment[] {Segment.newRoad(0)};
+        Segment[] fieldSegments = new Segment[] {Segment.newField(0), Segment.newField(1)};
+        Segment cloisterSegment = null;
+        int[] cities = new int[] {-1, 0, -1, -1};
+        int[] roads = new int[] {0, -1, 0, -1};
+        int[] fields = new int[] {0, 1, -1, -1, 1, 0, 0, 0};
+        return new Tile(id, name, borderTypes,
+                citySegments, roadSegments, fieldSegments, cloisterSegment,
+                cities, roads, fields);
     }
 
     public static Tile newE(int id) {
-	return null;
+        return null;
     }
 
     public static Tile newF(int id) {
-	return null;
+        return null;
     }
 
     public static Tile newG(int id) {
-	return null;
+        return null;
     }
 
     public static Tile newH(int id) {
-	return null;
+        return null;
     }
 
     public static Tile newI(int id) {
-	return null;
+        return null;
     }
 
     public static Tile newJ(int id) {
-	return null;
+        return null;
     }
 
     public static Tile newK(int id) {
-	return null;
+        return null;
     }
 
     public static Tile newL(int id) {
-	return null;
+        return null;
     }
 
     public static Tile newM(int id) {
-	return null;
+        return null;
     }
 
     public static Tile newN(int id) {
-	return null;
+        return null;
     }
 
     public static Tile newO(int id) {
-	return null;
+        return null;
     }
 
     public static Tile newP(int id) {
-	return null;
+        return null;
     }
 
     public static Tile newQ(int id) {
-	return null;
+        return null;
     }
 
     public static Tile newR(int id) {
-	return null;
+        return null;
     }
 
     public static Tile newS(int id) {
-	return null;
+        return null;
     }
 
     public static Tile newT(int id) {
-	return null;
+        return null;
     }
     
     public static Tile newU(int id) {
-	return null;
+        return null;
     }
     
     public static Tile newV(int id) {
-	return null;
+        return null;
     }
     
     public static Tile newW(int id) {
-	return null;
+        return null;
     }
 
     public static Tile newX(int id) {
-	return null;
+        return null;
     }
 
     public static Tile newFromName(int id, String name) {
-	return null;
+        return null;
     }
 }
