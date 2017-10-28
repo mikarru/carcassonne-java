a78b452667e8608dd59ee98cfaccfa7cb3c68f53
Yuki Tawara <yktawara@gmail.com>
2017-10-29 03:28:15 +0900

    Remove unused import

--- src/test/java/com/example/carcassonne/CloisterRegionTest.java
+++ src/test/java/com/example/carcassonne/CloisterRegionTest.java
@@ -3,7 +3,6 @@ package com.example.carcassonne;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-import org.junit.Before;
 import org.junit.Test;
 
 
